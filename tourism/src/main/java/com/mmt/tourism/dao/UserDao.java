68c8b21019bf1335d5b90e5f41fcf765bf52a1c8
JRD <1477450172@qq.com>
2017-01-15 16:38:30 +0800

    1/15晚 user创建

--- tourism/src/main/java/com/mmt/tourism/dao/UserDao.java
+++
@@ -1,8 +0,0 @@
-package com.mmt.tourism.dao;
-
-import com.mmt.tourism.pojo.User;
-
-public interface UserDao {
-
-	User getUserByName(String username);
-}
--- tourism/src/main/java/com/mmt/tourism/service/impl/UserServiceImpl.java
+++ tourism/src/main/java/com/mmt/tourism/service/impl/UserServiceImpl.java
@@ -3,15 +3,16 @@ package com.mmt.tourism.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.mmt.tourism.dao.UserDao;
+import com.mmt.tourism.dao.UserMapper;
 import com.mmt.tourism.pojo.User;
+import com.mmt.tourism.pojo.UserExample;
 import com.mmt.tourism.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService{
 	
 	@Autowired
-	private UserDao userDao;
+	private UserMapper userMapper;
 
 	@Override
 	public User getUserByName_Pwd(User user) {
@@ -21,9 +22,10 @@ public class UserServiceImpl implements UserService{
 
 	@Override
 	public Boolean getUserByName(String username) {
-		
-		User u=userDao.getUserByName(username);
-		if(u!=null)
+		UserExample userExample=new UserExample();
+		userExample.createCriteria().andUsernameEqualTo(username);
+		int count=userMapper.countByExample(userExample);
+		if(count>0)
 			return true;
 		return false;
 		
