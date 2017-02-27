2b723dac0e1c228d1394a71857c6c5cc8950d4f3
JRD <1477450172@qq.com>
2017-02-27 20:43:46 +0800

    修改

---
+++ tourism/src/test/java/sql/provinceData.sql
@@ -0,0 +1,4 @@
+USE traveling;
+INSERT INTO t_province(name,nameCode) VALUES('安徽省','AH');
+INSERT INTO t_province(name,nameCode) VALUES('江苏省','JS');
+INSERT INTO t_province(name,nameCode) VALUES('浙江省','ZJ');
